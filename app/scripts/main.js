367acc2c45c5feeb3b5a13bc7106fe0eaea0f1a2
Swashcap <swashcap@gmail.com>
2014-05-10 12:30:48 -0700

    Move Require.js configuration to main script file.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,19 @@
-require(['backbone'], function(Backbone) {
-  console.log('Ready to Backbone.');
+/* global requirejs */
+requirejs.config({
+  baseUrl: '/scripts/',
+  paths: {
+    'jquery': '/bower_components/jquery/dist/jquery',
+    'underscore': '/bower_components/underscore/underscore',
+    'backbone': '/bower_components/backbone/backbone',
+    'text': '/bower_components/requirejs-text/text'
+  },
+  shim: {
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'underscore': {
+      exports: '_'
+    },
+  }
 });
