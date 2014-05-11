03012ec49d7953a0828dc0d08e43a371d00cf99d
Swashcap <swashcap@gmail.com>
2014-05-11 16:43:40 -0700

    Add sample data.

---
+++ app/scripts/sample-data.js
@@ -0,0 +1,45 @@
+/* global define */
+define({
+  entries: [{
+    id: 1,
+    date: 1399686832392,
+    duration: 2.25
+  }, {
+    id: 2,
+    date: 1399686832392,
+    duration: 6
+  }, {
+    id: 3,
+    date: 1399686832392,
+    duration: 0.5
+  }],
+  days: [{
+    id: 101,
+    date: 1399264885339,
+    duration: 0
+  }, {
+    id: 102,
+    date: 1399351285339,
+    duration: 6.25
+  }, {
+    id: 103,
+    date: 1399437685339,
+    duration: 0
+  }, {
+    id: 104,
+    date: 1399524085339,
+    duration: 0
+  }, {
+    id: 105,
+    date: 1399610485339,
+    duration: 5.5
+  }, {
+    id: 106,
+    date: 1399696885339,
+    duration: 4.25
+  }, {
+    id: 107,
+    date: 1399610485339,
+    duration: 0
+  }]
+});
